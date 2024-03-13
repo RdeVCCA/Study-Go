7c275e6a87789eb287df699da2f22c0830c71760
Segfault (Core dumped) <tyiwei0@gmail.com>
2024-03-13 22:05:06 +0800

    Basics of refactor

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 
-import "buttons/start_button.dart";
-import "layout/desktop_homepage.dart";
-
 void main() {
   runApp(
     const StudyGo(),
@@ -29,52 +26,37 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final List<Color> colours = [
-      const Color(0x1f1e33ff),
-      const Color(0xfbd6c7ff)
-    ];
-
     return Scaffold(
-      floatingActionButton: const StartButton(),
       body: LayoutBuilder(
-        builder: (BuildContext context, BoxConstraints constraints) {
-          final double width = constraints.maxWidth - constraints.minWidth;
-
-          if (constraints.maxWidth > 900) {
-            /* desktop */
-
-            return DesktopHomepage();
-
-            /* enddesktop */
-          } else {
-            /* Mobile or thin desktop */
-
-            return Scaffold(
-              floatingActionButton: const StartButton(),
-              body: Container(
-                height: 200,
-                padding: const EdgeInsetsDirectional.all(16),
-                alignment: Alignment.center,
-                child: ListView.separated(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: 2,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Container(
-                      width: width / 2 - 32,
-                      color: colours[index],
-                      padding: const EdgeInsets.all(8),
-                      child: const Center(child: Text('Text')),
-                    );
-                  },
-                  separatorBuilder: (context, index) => Container(
-                    width: 16,
-                    color: const Color(0xffffffff),
+        builder: (context, constraints) {
+          if ((constraints.maxWidth - constraints.minWidth) > 900) {
+            return Row(
+              children: <Widget>[
+                Container(
+                  alignment: Alignment.topLeft,
+                  child: Card(
+                    child: Text("aoeu"),
                   ),
                 ),
-              ),
+                Column(
+                  children: <Widget>[
+                    Container(
+                      alignment: Alignment.topLeft,
+                      child: Card(
+                        child: Text("snth"),
+                      ),
+                    ),
+                    Container(
+                      alignment: Alignment.topLeft,
+                      child: Text("snth"),
+                    ),
+                  ],
+                )
+              ],
             );
-
-            /* endmobile */
+          } else {
+            return // mobile
+                Container(child: Text("AOESAOETNUHOSENTUONSETU"));
           }
         },
       ),
