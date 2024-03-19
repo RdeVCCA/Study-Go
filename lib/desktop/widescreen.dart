0f2eb4e9fffea79e814692e44170483e9fe109b0
Segfault (Core dumped) <tyiwei0@gmail.com>
2024-03-19 12:02:18 +0800

    Completed leaderboard with placeholder values.

--- lib/desktop/widescreen.dart
+++ lib/desktop/widescreen.dart
@@ -1,4 +1,9 @@
+import 'dart:collection';
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+
+import 'leaderboard.dart';
 
 class Widescreen extends StatelessWidget {
   const Widescreen({
@@ -25,53 +30,6 @@ class Widescreen extends StatelessWidget {
           ],
         )
       ],
-    );
-  }
-}
-
-class Leaderboard extends StatelessWidget {
-  const Leaderboard({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 700,
-      alignment: Alignment.topLeft,
-      child: Card(
-        margin: const EdgeInsets.all(8),
-        child: SizedBox(
-          child: ListView.separated(
-            padding: const EdgeInsets.fromLTRB(8, 10, 8, 10),
-            scrollDirection: Axis.vertical,
-            shrinkWrap: true,
-            separatorBuilder: (context, index) => const Divider(
-              thickness: 1,
-            ),
-            itemBuilder: (context, index) => Container(
-              alignment: Alignment.topLeft,
-              child: Row(
-                children: <Widget>[
-                  SizedBox(
-                    child: Text("Gay!"),
-                    width: 500,
-                  ),
-                  Container(
-                    alignment: Alignment.centerRight,
-                    width: 100,
-                    child: Text(
-                      "aoeuaoeu",
-                      textAlign: TextAlign.right,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            itemCount: 15,
-          ),
-        ),
-      ),
     );
   }
 }
