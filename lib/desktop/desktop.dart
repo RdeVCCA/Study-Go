9626118ebe3ae3e75533747baff17e1e4434ee35
Segfault (Core dumped) <tyiwei0@gmail.com>
2024-03-21 12:28:56 +0800

    Moved appbar to its own file

--- lib/desktop/desktop.dart
+++ lib/desktop/desktop.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'widescreen.dart';
+import 'widescreen/app_bar.dart';
 
 enum Menu {
   account,
@@ -34,55 +35,6 @@ class _DesktopState extends State<Desktop> {
           },
         ),
       ),
-    );
-  }
-}
-
-class DesktopAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const DesktopAppBar({
-    super.key,
-  });
-
-  @override
-  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
-
-  @override
-  Widget build(BuildContext context) {
-    return AppBar(
-      title: const Text("Study Go! (Name to be determined"),
-      backgroundColor: const Color(0xffe4e2fe),
-      actions: <Widget>[
-        PopupMenuButton<Menu>(
-          icon: const Icon(Icons.settings),
-          onOpened: () {},
-          onCanceled: () {},
-          onSelected: (value) {
-            switch (value) {
-              case Menu.settings:
-                // Navigator is static, push to it to open a new page when implementing settings
-                break;
-
-              case Menu.account:
-                break;
-
-              // case _:
-              //   break;
-            }
-          },
-          itemBuilder: (context) {
-            return [
-              const PopupMenuItem(
-                value: Menu.settings,
-                child: Text("Settings"),
-              ),
-              const PopupMenuItem(
-                value: Menu.account,
-                child: Text("Account"),
-              )
-            ];
-          },
-        ),
-      ],
     );
   }
 }
