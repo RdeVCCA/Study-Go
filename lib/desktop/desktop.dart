bb1900261d65a24739837c54a5a8cd45f779d95e
Segfault (Core dumped) <tyiwei0@gmail.com>
2024-03-27 05:33:48 +0000

    Created narrow screen

--- lib/desktop/desktop.dart
+++ lib/desktop/desktop.dart
@@ -3,6 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:app0000/desktop/widescreen.dart';
 import 'package:app0000/desktop/widescreen/app_bar.dart';
 import 'package:app0000/desktop/widescreen/start_button.dart';
+import 'package:flutter/rendering.dart';
+
+import 'narrow_screen.dart';
+import 'narrow_screen/leaderboard.dart';
+import 'narrow_screen/stopwatch.dart';
+import 'narrow_screen/sys_score.dart';
 
 class Desktop extends StatefulWidget {
   const Desktop({
@@ -24,7 +30,7 @@ class _DesktopState extends State<Desktop> {
             if ((constraints.maxWidth - constraints.minWidth) > 900) {
               return const Center(child: Widescreen());
             } else {
-              return const Text("AOESAOETNUHOSENTUONSETU");
+              return const Center(child: NarrowScreen());
             }
           },
         ),
